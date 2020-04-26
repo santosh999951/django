b13429faf69a971e260e196703253f3a322a035a
santosh singhal <singhalsantosh9@gmail.com>
2020-04-26 14:33:57 +0530

    Reset password and update Profile Images

--- users/models.py
+++ users/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
+
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile = models.ImageField()
+
+    def save(self):
+        super().save()
+        image = Image.open(self.profile.path)
+        if image.height > 400 or image.width > 400:
+            output_size = (400, 400)
+            image.thumbnail(output_size)
+            image.save(self.profile.path)
+
+
